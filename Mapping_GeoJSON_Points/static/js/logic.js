ce4a33fd1f1184483662b65525209f2468eb0d2e
shanl5 <106628649+shanl5@users.noreply.github.com>
2022-09-02 23:16:18 -0400

    Update file for mapping GeoJSON points.
    Mod. 13.5.2 onEachFeature callback function

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,7 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
 // 13.5.1 : set map center to SFO, zoom level 10
 let map = L.map('mapid').setView([37.5, -122.5], 10);
 
@@ -27,20 +26,17 @@ let sanFranAirport =
 
 // // Add feature to map.
 // L.geoJSON(sanFranAirport).addTo(map);
-// put more data to map with "`pointToLayer` callback function" 
-// "Grabbing our GeoJSON data."
+// ---------
+// put more data to map with "`onEachFeature` callback function"
 L.geoJSON(sanFranAirport, {
-    // "We turn each feature into a marker on the map."
-    pointToLayer: function(feature, latlng) {
-        console.log(feature);
-        return L.marker(latlng)
-        // .bindPopup("<h2>" + feature.properties.city + "</h2>");
-        .bindPopup("<h2>" + feature.properties.name + "</h2><hr><h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>")
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h4>Airport code: " + feature.properties.faa + "<hr>" + "Airport name: " + feature.properties.name) + "</h4>";
     }
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets2 = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets2 = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
